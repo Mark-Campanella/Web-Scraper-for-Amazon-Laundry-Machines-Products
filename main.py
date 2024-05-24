82c50c5a30223353dfcb577f9a18e2f4dd4c88c0
MARCOS  CAMPANELLA <mhmcamp@gmail.com>
2024-05-24 14:10:09 -0300

    IDK

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ import pandas as pd
 
 # -----------------------------------------Customization Variables---------------------------------------------------
 
-url = "https://www.amazon.com.br"  # Where you want to scrape, change to us, uk, etc. sites to change locations!
-keywords = "Lavadora e Secadoraa de Roupas"  # The topic you want to scrape, Misspelling? Yes, I dunno why but this is the best results
+url = "https://www.amazon.com.br"
+keywords = "Lavadora e Secadoraa de Roupas"
 
 # Navigation classes
 class_search_bar = 'nav-input.nav-progressive-attribute'
@@ -25,17 +25,15 @@ class_itens = 's-result-item.s-asin'
 class_link = 'a-link-normal.s-underline-text.s-underline-link-text.s-link-style.a-text-normal'
 
 # Item classes DEPRECATED 
-att_img = 'data-image-source-density'  # search by tag name --to do
-class_local_currency = "a-price-symbol" # Do we need this?
-
-
+att_img = 'data-image-source-density'
+class_local_currency = "a-price-symbol"
 
 # For these ones we would need to be inside the products page
 class_name = 'a-size-large.product-title-word-break'
 class_price = "a-price-whole"
 class_marketing_claims_div = "productDescription"
-xpath_prod_description = '//*[@id="productDescription"]/p/span/text()' #the only one I am using XPATH because it was tooo hard to find it by class
-class_model_description = "a-size-large.product-title-word-break" #testign...
+xpath_prod_description = '//*[@id="productDescription"]/p/span/text()'
+xpath_img_1 = ''
 class_th = "a-color-secondary.a-size-base.prodDetSectionEntry"
 class_td = 'a-size-base.prodDetAttrValue'
 class_5_Star = 'reviewCountTextLinkedHistogram.noUnderline'
@@ -54,25 +52,20 @@ chrome_options.add_argument("--disable-notifications")
 chrome_options.add_argument("--disable-popup-blocking")
 chrome_options.add_argument("--incognito")
 chrome_options.add_argument("--disable-extensions")
-
-languages=["pt-BR", "pt"] 
-
+languages = ["pt-BR", "pt"]
 driver = webdriver.Chrome(options=chrome_options)
 
 # configure Selenium Stealth
 stealth(driver,
-        languages,
+        languages=languages,
         vendor="Google Inc.",
         platform="Win32",
         webgl_vendor="Intel Inc.",
         renderer="Intel Iris OpenGL Engine",
         fix_hairline=True)
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
-
+#-----------------------------------------------------------------------------------FUNCTIONS----------------------------------------------------------------#
 def scrape_page(driver):
-    '''
-    Gets all links from all objects in the website (it automatically changes the page)
-    '''
     global product_link
     global next_page
 
@@ -86,28 +79,16 @@ def scrape_page(driver):
         item = element.find_element(By.CLASS_NAME, class_link)
         product_link.append(item.get_attribute('href'))
 
-    # # try to find a way to the next page
+    # Uncomment after testing:
     # try:
     #     next_page = driver.find_element(By.CLASS_NAME, class_next_btn).get_attribute("href")
     # except:
     #     next_page = None
 
-
-def process_product(driver, link:str):
-    '''
-    Access it, get info of the object, go to the next
-    
-    Parameters:
-        driver: Web
-        link: str
-    '''
+def process_product(driver, link):
     global products_data
-    try:
-        driver.get(link)
-        driver.implicitly_wait(20)
-    except:
-        print("Could not get into object")
-        return
+    driver.get(link)
+    driver.implicitly_wait(20)
 
     product_info = {'product_link': link}
 
@@ -124,61 +105,48 @@ def process_product(driver, link:str):
         product_info['product_price'] = ""
 
     try:
-        #Get elements from the table
         th_elements = driver.find_elements(By.CLASS_NAME, class_th)
         td_elements = driver.find_elements(By.CLASS_NAME, class_td)
         th_texts = [elem.text for elem in th_elements]
         td_texts = [elem.text for elem in td_elements]
 
         try:
-            five_stars = (driver.find_element(By.CLASS_NAME,class_5_Star)).get_attribute('title')
+            five_stars = driver.find_element(By.CLASS_NAME, class_5_Star).get_attribute('title')
         except:
             five_stars = None
 
-        spec = {}
         for th_text, td_text in zip(th_texts, td_texts):
             if th_text == "Avaliações de clientes":
-                spec[th_text] = five_stars
+                product_info[th_text] = five_stars
             elif th_text == "Ranking dos mais vendidos":
-                spec[th_text] = ""
+                product_info[th_text] = ""
             else:
-                spec[th_text] = td_text
-        product_info['specs'] = spec
+                product_info[th_text] = td_text
     except Exception as e:
         print("Error: ", e)
-        product_info['specs'] = {}
-        
     try:
-        marketing_claims = driver.find_element(By.XPATH,)
+        description = driver.find_elements(By.CLASS_NAME,xpath_prod_description).text
     except:
-        marketing_claims = None
+        description = None
     finally:
-        product_info['Marketing Claims'] = marketing_claims
+        product_info["Description"] = description
+        
     products_data.append(product_info)
 
-
 def process_products(driver):
-    '''
-    For each object → process_product
-    '''
     global product_link
     for link in product_link:
         process_product(driver, link)
     product_link.clear()
 
-
-# Iniciar o scraping
+# Start!
 try:
-    driver.get(url)  # Go to the Website
+    driver.get(url)
     driver.implicitly_wait(20)
-    # Find search bar
     search = driver.find_element(By.CLASS_NAME, class_search_bar)
-    # Type: lavadora e secadora de roupas
     search.send_keys(keywords)
-    # Find search button
     time.sleep(1)
     search_btn = driver.find_element(By.ID, class_submit_btn)
-    # Click on the search button
     search_btn.click()
     driver.implicitly_wait(5)
     while True:
@@ -191,11 +159,9 @@ try:
     process_products(driver)
 
 finally:
-    # Fechar o driver
     driver.quit()
 
     # Converter a lista de dicionários em um DataFrame
     df = pd.DataFrame(products_data)
     print(df)
     df.to_csv('product_data.csv', index=False)
-    
